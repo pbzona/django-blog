a763751a2e91d0f1859c5182878b564f1ea60506
Phil Zona <phil.b.zona@gmail.com>
2023-07-27 23:05:49 -0400

    Initial migration for blog, adds author relation

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class Post:
+class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
 
     publish = models.DateTimeField(default=timezone.now)
