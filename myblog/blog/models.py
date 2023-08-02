d79c2ef5b17cfb923caf3cd12db82e3b11dda84d
Phil Zona <phil.b.zona@gmail.com>
2023-08-01 22:49:40 -0400

    Use canonical url

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 from taggit.managers import TaggableManager
 
 
@@ -40,3 +41,6 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
